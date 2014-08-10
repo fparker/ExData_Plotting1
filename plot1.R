f98b522122b28275b43b159c54ce4c548479b609
Frank E. Parker <fparker@jcsu.edu>
2014-08-10 18:43:40 -0400

    corrected update

---
+++ plot1.R
@@ -0,0 +1,16 @@
+setwd("~/GitHub/Exdata-005/exdata-data-household_power_consumption")
+household_power_consumption <- read.table("household_power_consumption.txt",head=TRUE,sep=";",
+                                          stringsAsFactors=FALSE)
+consumption<-household_power_consumption[,c(1:9)]
+y<-subset(consumption, 
+          consumption$Date=="1/2/2007" | consumption$Date=="2/2/2007")
+
+##png("plot1.png", width = 480, height = 480)
+hist(as.numeric(y$Global_active_power), 
+     breaks=20, 
+     freq=TRUE, 
+     ylim=c(0, 1300),
+     col="red",
+     main="Global Active Power",
+     xlab="Global Active Power (kilowatts)")
+dev.off()
