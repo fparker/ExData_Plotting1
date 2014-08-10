4ab10233a61bb7cc1a91dfa612c06cf46728ce5d
Frank E. Parker <fparker@jcsu.edu>
2014-08-10 18:48:33 -0400

    update corrected copy, uncommented file write

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ consumption<-household_power_consumption[,c(1:9)]
 y<-subset(consumption, 
           consumption$Date=="1/2/2007" | consumption$Date=="2/2/2007")
 
-##png("plot1.png", width = 480, height = 480)
+png("plot1.png", width = 480, height = 480)
 hist(as.numeric(y$Global_active_power), 
      breaks=20, 
      freq=TRUE, 
